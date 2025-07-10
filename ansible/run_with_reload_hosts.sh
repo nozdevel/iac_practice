49ef20a0d918a9521ab78bc6e7b68601b7eff733
nozdevel <noz.devel@gmail.com>
2025-07-11 01:20:36 +0900

    RTSP EC2のIPをknown_hostsに追加する処理をスクリプトに追加

--- ansible/run_with_reload_hosts.sh
+++ ansible/run_with_reload_hosts.sh
@@ -8,5 +8,11 @@ export AWS_REGION=${AWS_REGION:-ap-northeast-1}
 # インベントリを最新化
 python3 scripts/update_rtsp_inventory.py
 
+# RTSP EC2のIPをknown_hostsに追加
+rm -f ~/.ssh/known_hosts
+for ip in $(awk '/^[0-9]+\./ {print $1}' inventory/hosts); do
+  ssh-keyscan -H $ip >> ~/.ssh/known_hosts 2>/dev/null
+don
+
 # Playbookを実行
 ansible-playbook -i inventory/hosts playbook.yml
